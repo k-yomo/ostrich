4fb4a8daa5216565c233dfca3739172cc61f1c45
k-yomo <k-yomoda@mercari.com>
2022-12-02 22:13:36 +0900

    Add tests for memory directory's delete

--- directory/memory_test.go
+++ directory/memory_test.go
@@ -103,3 +103,47 @@ func Test_memoryDirectory_OpenWrite(t *testing.T) {
 		})
 	}
 }
+
+func Test_memoryDirectory_Delete(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name            string
+		memoryDirectory *memoryDirectory
+		args            args
+		wantErr         bool
+	}{
+		{
+			name: "delete path",
+			memoryDirectory: func() *memoryDirectory {
+				m := NewMemoryDirectory()
+				m.AtomicWrite("test", []byte{})
+				return m
+			}(),
+			args: args{path: "test"},
+		},
+		{
+			name:            "delete not existing path won't cause error",
+			memoryDirectory: NewMemoryDirectory(),
+			args:            args{path: "not_exist"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.memoryDirectory.Delete(tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			exists, err := tt.memoryDirectory.Exists(tt.args.path)
+			if err != nil {
+				t.Errorf("Delete(), Exists() error = %v, wantErr %v", err, false)
+			}
+			if exists {
+				t.Errorf("Delete(), Exists() got = %v, want %v", exists, false)
+			}
+		})
+	}
+}
